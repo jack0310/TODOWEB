0f4b2f5f3b1c438c753df10d7023d3c0a6bc8897
jack <jack2780jack@gmail.com>
2023-08-18 20:49:24 +0900

    local storage에 데이터 저장 및 불러오기

--- TODOWEB/todo.js
+++ TODOWEB/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo() {
     console.log(event.target.parentElement);
     const li = event.target.parentElement;
@@ -24,7 +32,16 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if (savedToDos) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
